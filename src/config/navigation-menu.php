4cc391e3a14cf09a2dfb7e4b4ee706e6401aaa51
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2023-10-31 11:25:03 +0100

    sync menu

--- src/config/navigation-menu.php
+++ src/config/navigation-menu.php
@@ -4,6 +4,11 @@ $menuAdmin = [
         'title' => 'Marketplace Settings',
         'route' => 'admin-marketplace',
         'icon' => 'fe fe-file-text'
+    ],
+    'admin-sync' => [
+        'title' => 'Synchronization Settings',
+        'route' => 'admin-sync',
+        'icon' => 'fe fe-file-text'
     ]
 ];
 
